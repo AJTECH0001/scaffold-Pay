f9016f4d6b7e4fc8d2ac2168b264fbba44cf2909
AJTECH <aladejamiudamilola@gmail.com>
2024-08-16 10:44:03 +0100

    code updated

--- packages/nextjs/components/hero/Hero.jsx
+++ packages/nextjs/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Image from 'next/image'
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 // import icons
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 
@@ -17,25 +17,15 @@ const Hero = () => {
             data-aos-delay="400"
           >
             <div className="flex items-center justify-between text-sm lg:text-base">
-              <div className="bg-white text-darkblue rounded-full p-1 px-4 font-medium">
-                75% SAVE
-              </div>
+              <div className="bg-white text-darkblue rounded-full p-1 px-4 font-medium">75% SAVE</div>
               <div>For the Black Friday weekend</div>
             </div>
           </div>
           {/* title */}
-          <h1
-            className="text-[32px] lg:text-[64px] leading-tight mb-6 "
-            data-aos="fade-down"
-            data-aos-delay="500"
-          >
+          <h1 className="text-[32px] lg:text-[64px] leading-tight mb-6 " data-aos="fade-down" data-aos-delay="500">
             Fastest & secure platform to manage your crypto.
           </h1>
-          <p
-            className="max-w-[440px] leading-relaxed mb-8"
-            data-aos="fade-down"
-            data-aos-delay="600"
-          >
+          <p className="max-w-[440px] leading-relaxed mb-8" data-aos="fade-down" data-aos-delay="600">
             Crypto Savings & Bills Management Simplified
           </p>
           <button
@@ -43,26 +33,17 @@ const Hero = () => {
             data-aos="fade-down"
             data-aos-delay="700"
           >
-             <Link href={'/dapp'}>
-             <button className="flex items-center justify-betweeen gap-2">
-              GET STARTED
-              <IoIosArrowDroprightCircle className="text-2xl lg:text-3xl" />
-            </button>
-             </Link>
-           
-            
-           
+            <Link href={"/dapp"}>
+              <button className="flex items-center justify-betweeen gap-2">
+                GET STARTED
+                <IoIosArrowDroprightCircle className="text-2xl lg:text-3xl" />
+              </button>
+            </Link>
           </button>
         </div>
         {/* Hero image flex-col2nd element */}
         <div className="flex-1">
-       
-        <Image
-      src="/hero-img.png"
-      width={500}
-      height={500}
-      alt="Picture of the author"
-    />
+          <Image src="/hero-img.png" width={500} height={500} alt="Picture of the author" />
         </div>
       </div>
     </section>
