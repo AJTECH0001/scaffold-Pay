e6561fdd5c61d6ee649907343a798736306e2a41
AJTECH <aladejamiudamilola@gmail.com>
2024-08-16 12:04:43 +0100

    updated calculate.jsx

--- packages/nextjs/components/sections/Calculate.jsx
+++ packages/nextjs/components/sections/Calculate.jsx
@@ -4,23 +4,23 @@ import CalcForm from "./CalcForm";
 
 const Calculate = () => {
   return (
-    <section className="section">
+    <section className="">
       <div className="container mt-10 mx-auto">
         {/* text */}
         <div className="text-center">
           <h2
-            className="section-title"
+            className=""
             data-aos="fade-up"
             data-aos-offset="400"
           >
             Check how much you can earn.
           </h2>
           <p
-            className="section-subtitle text-lg mb-16 max-w-[622px] mx-auto"
+            className=" text-lg mb-16 max-w-[622px] mx-auto"
             data-aos="fade-up"
             data-aos-offset="450"
           >
-            Let's check your hash rate to see how much you will earn today.
+            Let&apos;s check your hash rate to see how much you will earn today.
           </p>
         </div>
         {/* form */}
