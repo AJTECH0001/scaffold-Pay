e086be7f64ba0399c7e66cbe7ef72c73229054ba
AJHECH <aladejamiudamilola@gmail.com>
2024-08-31 01:07:17 -0700

    updated callContract ui page

--- packages/nextjs/callContract/AddBill.tsx
+++ packages/nextjs/callContract/AddBill.tsx
@@ -41,7 +41,7 @@ export function AddBill() {
   return (
     <div className="max-w-sm mx-auto">
       <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg ease-linear transition-all duration-150"
+        className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg ease-linear transition-all duration-150"
         type="button"
         onClick={() => setShowModal(true)}
       >
--- packages/nextjs/callContract/Deposit.tsx
+++ packages/nextjs/callContract/Deposit.tsx
@@ -39,10 +39,10 @@ export function Deposit() {
 
   return (
     <div className="max-w-sm mx-auto">
-      <h1 className="text-2xl text-center font-bold mb-4">DEPOSIT</h1>
       <button
+        className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg ease-linear transition-all duration-150"
+        type="button"
         onClick={() => setIsModalOpen(true)}
-        className="bg-blue-500 px-6 py-2 rounded-full text-white text-sm lg:h-16 lg:text-base font-bold gap-x-6"
       >
         Deposit
       </button>
